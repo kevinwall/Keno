4277f99ef6f0d7449fe7da89dc46c361b58cded5
GSDante <giovannedssantos@gmail.com>
2018-10-04 11:26:20 -0300

    leitura

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,32 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 #include "../include/gamemanager.h"
 
 int main(int argc, char const *argv[])
 {
-	
+	std::ifstream file(argv[1]);
+	std::string string;
+	long int money;
+	int number_of_bet, count;
+
+	if( agrc != 2){
+		std::cerr << " Número de argumentos invalidos\n";
+	}
+
+	if(file.is_open()){
+		file >> money;
+		file >> number_of_bet;
+		getline(file,string);
+	}
+
+	std::ostringstream oss(string); 
+	int A[15], i = 0;
+
+	while( oss.empty()){
+		oss >> A[i];
+		i++
+	}
 
 	return 0;
 }
