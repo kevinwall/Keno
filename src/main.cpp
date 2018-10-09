36aa0b2361955f30e71ccb06a58e9284caa90d81
KEVIN WALLACY DE SOUZA MACIEL <kevinwall@A307-09L.imd.local>
2018-10-09 09:56:25 -0300

    Leitura do arquivo implementada

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <sstream>
 //#include "../include/gamemanager.h"
 
 int main(int argc, char const *argv[])
@@ -8,31 +9,52 @@ int main(int argc, char const *argv[])
 	std::ifstream file(argv[1]);
 	std::string string;
 	long int money;
-	int number_of_bet, count;
+	int number_of_bet;
 
-	if( agrc != 2){
+	if( argc != 2){
 		std::cerr << " Número de argumentos invalidos\n";
+		return 0;
 	}
 
-	if(file.is_open()){
-		file >> money;
-		file >> number_of_bet;
-		getline(file,string);
+	if(!file.is_open()){
+		std::cerr << " Arquivo vazio\n";
+		return 0;	
 	}
 
-	std::ostringstream oss(string); 
+	getline(file,string);
+	std::stringstream oss(string); 
+	std::cout <<"String atual: "<<string<<std::endl;
+	oss >> money;
+	oss.clear();
+
+	getline(file,string);
+	oss.str(string);
+	std::cout <<"String atual: "<<string<<std::endl;
+	oss >> number_of_bet;
+	oss.clear();
+
+	getline(file,string);
+	oss.str(string);
+	std::cout <<"String atual: "<<string<<std::endl;
+
 	int A[15], i = 0;
 
-	while( oss.empty()){
+
+	while(oss.good())
+	{
 		oss >> A[i];
 		i++;
 	}
 
-	std::cout << "Dinheiro:" << money << "\n";
-	std::cout << "Numero de apostas:" << number_of_bet << "\n";
-	for( auto j = 0 ; j < i ; j++){
-		std::cout << "Apostas:" << A[j] << " ";
+	std::cout << "Dinheiro: " << money << "\n";
+	std::cout << "Numero de apostas: " << number_of_bet << "\n";
+
+	std::cout<<"Vetor de apostas: [";
+	for(auto j{0}; j < i; j++)
+	{
+		std::cout << A[j] << " ";
 	}
+	std::cout <<"]"<<std::endl;
 
 	return 0;
 }
